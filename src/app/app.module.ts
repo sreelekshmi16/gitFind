337553a925bb28fa8e7d6e823c38fb641d5f6634
sreelekshmi16 <sreelekshmi.gs@experionglobal.com>
2019-09-29 14:04:54 +0530

    gitfind app updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { DisplayComponent } from './display/display.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    DisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/content/content.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+  userName: string;
+  prof;
+   success: boolean;
+  private baseUrl = 'https://api.github.com/users/';
+  constructor(private httpClient: HttpClient, private snackBar: MatSnackBar, private spinner: MatProgressSpinnerModule) { }
+
+  getProfile() {
+    this.success = false;
+    this.httpClient.get(this.baseUrl + this.userName + `?access_token=8d37aa24a19f6f9467b2752a02e9459c90c82e88 `).subscribe((res) => {
+      this.success = true;
+      this.prof = res;
+      if (localStorage.getItem(this.userName)) {
+        JSON.parse( localStorage.getItem(this.userName));
+      }
+      else {
+        localStorage.setItem(this.userName, JSON.stringify(this.prof));
+        console.log(localStorage);
+        // console.log(this.getItemFromProfile);
+        console.log(localStorage);
+      }
+      // console.log(this.prof);
+    },
+      () => {
+        this.success = true;
+        this.snackBar.open('not found', ' ' , {
+          duration: 2000,
+        });
+      });
+  }
+
+
+  ngOnInit() {
+    this.success =  true;
+  }
+}
